08b85bef1b63d8f26e3ec3cb1cab591421daed24
Jaemin Hong <hjm0901@gmail.com>
2019-06-09 20:25:27 +0200

    add kr/3

--- format/Format.scala
+++ format/Format.scala
@@ -6,34 +6,52 @@ import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
 
 object Format {
-  def main(args: Array[String]): Unit = args match {
-    case Array(name, lang) =>
+  private def read(name: String): String =
+    Source.fromFile(name).mkString.filter(c => c.isLetter || c.isDigit || c == ' ' || c == ':')
+
+  private def write(name: String, content: String): File = {
+    val file = new File(name)
+    val out = new FileOutputStream(file)
+    out.write(content.getBytes)
+    out.close()
+    file
+  }
+    
+
+  def main(args: Array[String]): Unit = args.toList match {
+    case name :: tail =>
+      val preview = tail.nonEmpty
+      val List(num, ver, title, lang) =
+        List("num", "ver", "title", "lang").map(read)
       val kr = lang == "kr"
-      val num = Source.fromFile(s"$name.num").mkString.filter(_.isDigit)
-      val ver = Source.fromFile(s"$name.ver").mkString.filter(_.isDigit)
+      val timeFile = new File("time")
+      val time = if (timeFile.exists) Some(read("time")) else None
+      val current = new SimpleDateFormat(dateFmt(kr)).format(new Date)
 
       val from = Source.fromFile(s"$name.md").mkString
-      val mdFile = new File(tempMd)
-      val md = new FileOutputStream(mdFile)
-      md.write(from
-        .replaceAll("\\\\", "\\\\\\\\")
-        .replace("today-date", new SimpleDateFormat(dateFmt(kr)).format(new Date))
-        .getBytes
+      val mdFile = write(tempMd,
+        header(title, time, current, kr) +
+        from.replaceAll("\\\\", "\\\\\\\\") +
+        footer(kr)
       )
-      md.close()
       Process(s"pandoc -s --css $lang.css -o $tempHtml $tempMd").!
 
       val htmlFile = new File(tempHtml)
       val html = Source.fromFile(htmlFile).mkString
-      val to = new FileOutputStream(s"${lang}_${num}_${ver}")
-      to.write(html
+      write(s"${lang}_${num}_${ver}",
+        html
         .replace("</head>", scripts(num, ver, kr))
         .replace("</header>", alertDiv)
-        .getBytes)
-      to.close()
+      )
 
       mdFile.delete()
       htmlFile.delete()
+
+      if (!preview) {
+        write("ver", (ver.toInt + 1).toString)
+        if (time.isEmpty)
+          write("time", current)
+      }
   }
 
   private val tempMd = "temp.md"
@@ -41,6 +59,32 @@ object Format {
 
   private def dateFmt(kr: Boolean) =
     if (kr) "y년 M월 d일 H시 m분" else "d MMMM yyyy HH:mm"
+  private def header(title: String, time: Option[String], current: String, kr: Boolean) =
+s"""% $title
+   |% ${if (kr) "홍재민" else "Jaemin Hong"}
+   |% ${if (time.isEmpty) current else time.get + s" (${if (kr) "최근 수정" else "Last update"}: $current)"}
+   |
+   |---
+   |
+   |""".stripMargin
+  private def footer(kr: Boolean) =
+    if (kr)
+"""|
+   |
+   |---
+   |
+   |감수: 류석영 교수님 (``sryu.cs@kaist.ac.kr``)
+   |
+   |글에 대한 질문과 의견은 ``jaemin.hong@kaist.ac.kr``로 부탁드립니다.""".stripMargin
+    else
+"""|
+   |
+   |---
+   |
+   |Edited under the supervision of Prof. Sukyoung Ryu (``sryu.cs@kaist.ac.kr``).
+   |
+   |If you have any question or opinion, please send an email to ``jaemin.hong@kaist.ac.kr``.""".stripMargin
+
   private def scripts(num: String, ver: String, kr: Boolean): String =
 s"""<script type="text/x-mathjax-config">
    |MathJax.Hub.Config({tex2jax: {
@@ -58,7 +102,7 @@ s"""<script type="text/x-mathjax-config">
    |.then(res => {
    |  let latest = parseInt(res.data.${if (kr) "kr" else "en"}_latest.N);
    |  if (latest > 0)
-   |    $$('#version-alert').append(`<p style="color:red;">${if (kr) "글이 최신 판이 아닙니다" else "Not the latest version"}.&nbsp;<a href="./${num}_$${latest}">${if (kr) "최신 판 보러가기" else "Go to the latest"}</a></p>`);
+   |    $$('#version-alert').append(`<p style="color:red;">${if (kr) "글이 최신 판이 아닙니다" else "Not the latest version"}.&nbsp;<a href="./${if (kr) "kr" else "en"}_${num}_$${latest}">${if (kr) "최신 판 보러가기" else "Go to the latest"}</a></p>`);
    |});
    |</script>
    |</head>""".stripMargin
--- make_new.sh
+++ make_new.sh
@@ -1,7 +1,13 @@
-if [ "$#" -ne 2 ] ; then
+if [ "$#" -ne 3 ] ; then
   echo "Invalid arguments"
   exit 1
 fi
 
-cp -R $1/template $1/$2
-echo $2 > $1/$2/index.num
+mkdir $1/$2
+cp template/Makefile $1/$2
+cp template/$1.css $1/$2
+echo 0 > $1/$2/ver
+echo $1 > $1/$2/lang
+echo $2 > $1/$2/num
+echo $3 > $1/$2/title
+touch $1/$2/index.md
